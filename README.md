dedf4b1cdfa1e73902e98a767957a4f5352725cd
unknown <xiaoxm_001@outlook.com>
2022-04-04 00:21:00 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fpdf-test
+
+解决目标: 0001.jpg 本来是png格式,但是被篡改成了jpg, 合并pdf时报错
---
+++ main.py
@@ -0,0 +1,37 @@
+'''
+解决目标: 0001.jpg 本来是png格式,但是被篡改成了jpg
+'''
+
+import os
+
+from fpdf import FPDF
+from PIL import Image
+
+
+def get_ext(img_path: str):
+    return Image.open(img_path).format
+
+
+def main():
+    try:
+
+        pdf = FPDF(orientation='L', unit='pt', format='legal')
+        pdf.set_auto_page_break(0)
+
+        width, height = 800, 500
+        folder_name = 'imgs'
+        for i in os.listdir(folder_name):
+            file_path = os.path.join(folder_name, i)
+            ext = get_ext(file_path)
+            pdf.add_page()
+            pdf.image(file_path, type=ext,
+                      w=width, h=height)
+        pdf.output('1.pdf', "F")
+        return 0
+    except Exception as e:
+        print(e)
+        return -1
+
+
+if __name__ == '__main__':
+    print(main())
